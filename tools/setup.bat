3605d07e70c81575186a42c8c00b9ef87544288d
peter88213 <peter.88213@t-online.de>
2021-03-29 18:07:59 +0200

    Move curly_de-CH to its own project

--- tools/setup.bat
+++ tools/setup.bat
@@ -3,14 +3,16 @@ copy ..\LICENSE ..\oxt\Legal\license-en.txt
 cd help
 call convert.bat
 cd ..
-xcopy /Y /s ..\oxt ..\oxt-de-CH\
-copy /Y ..\src\*.xba ..\oxt-de-CH\curly_de-CH\
+
 xcopy /Y /s ..\oxt ..\oxt-de-DE\
 copy /Y ..\src\*.xba ..\oxt-de-DE\curly_de-DE\
+
 xcopy /Y /s ..\oxt ..\oxt-en-GB\
 copy /Y ..\src\*.xba ..\oxt-en-GB\curly_en-GB\
+
 xcopy /Y /s ..\oxt ..\oxt-en-US\
 copy /Y ..\src\*.xba ..\oxt-en-US\curly_en-US\
+
 xcopy /Y /s ..\oxt ..\oxt-fr-FR\
 copy /Y ..\src\*.xba ..\oxt-fr-FR\curly_fr-FR\
 
--- tools/teardown.bat
+++ tools/teardown.bat
@@ -1,45 +1,45 @@
 rmdir /S /Q ..\oxt\help
-rmdir /S /Q ..\oxt-de-CH\Legal
-rmdir /S /Q ..\oxt-de-CH\help
-rmdir /S /Q ..\oxt-de-CH\Descriptions
-rmdir /S /Q ..\oxt-de-CH\icons
+
 rmdir /S /Q ..\oxt-de-DE\Legal
 rmdir /S /Q ..\oxt-de-DE\help
 rmdir /S /Q ..\oxt-de-DE\Descriptions
 rmdir /S /Q ..\oxt-de-DE\icons
+del ..\oxt-de-DE\curly_de-DE\Common.xba
+del ..\oxt-de-DE\curly_de-DE\Revert.xba
+
 rmdir /S /Q ..\oxt-en-GB\Legal
 rmdir /S /Q ..\oxt-en-GB\help
 rmdir /S /Q ..\oxt-en-GB\Descriptions
 rmdir /S /Q ..\oxt-en-GB\icons
+del ..\oxt-en-GB\curly_en-GB\Common.xba
+del ..\oxt-en-GB\curly_en-GB\Revert.xba
+
 rmdir /S /Q ..\oxt-en-US\Legal
 rmdir /S /Q ..\oxt-en-US\help
 rmdir /S /Q ..\oxt-en-US\Descriptions
 rmdir /S /Q ..\oxt-en-US\icons
+del ..\oxt-en-US\curly_en-US\Common.xba
+del ..\oxt-fr-FR\curly_fr-FR\Common.xba
+
 rmdir /S /Q ..\oxt-fr-FR\Legal
 rmdir /S /Q ..\oxt-fr-FR\help
 rmdir /S /Q ..\oxt-fr-FR\Descriptions
 rmdir /S /Q ..\oxt-fr-FR\icons
-
-del ..\oxt-de-CH\curly_de-CH\Common.xba
-del ..\oxt-de-DE\curly_de-DE\Common.xba
-del ..\oxt-en-GB\curly_en-GB\Common.xba
-del ..\oxt-en-US\curly_en-US\Common.xba
-del ..\oxt-fr-FR\curly_fr-FR\Common.xba
-
-del ..\oxt-de-CH\curly_de-CH\Revert.xba
-del ..\oxt-de-DE\curly_de-DE\Revert.xba
-del ..\oxt-en-GB\curly_en-GB\Revert.xba
 del ..\oxt-en-US\curly_en-US\Revert.xba
 del ..\oxt-fr-FR\curly_fr-FR\Revert.xba
 
-del ..\oxt-de-CH\*.oxt
+goto end
+
+del ..\..\curly_de-CH\*.oxt
 del ..\oxt-de-DE\*.oxt
 del ..\oxt-en-GB\*.oxt
 del ..\oxt-en-US\*.oxt
 del ..\oxt-fr-FR\*.oxt
 
-del ..\oxt-de-CH\*.xml
+del ..\..\curly_de-CH\*.xml
 del ..\oxt-de-DE\*.xml
 del ..\oxt-en-GB\*.xml
 del ..\oxt-en-US\*.xml
 del ..\oxt-fr-FR\*.xml
+
+:end
